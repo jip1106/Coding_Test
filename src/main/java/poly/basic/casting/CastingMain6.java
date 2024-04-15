662286fe590993f3c09319d8652fc77fd5017177
jipark1106 <jipark1106@gmail.com>
2024-04-15 14:12:03 +0900

    poly

---
+++ src/main/java/poly/basic/casting/CastingMain6.java
@@ -0,0 +1,25 @@
+package poly.basic.casting;
+import poly.basic.*;
+
+public class CastingMain6 {
+    public static void main(String[] args) {
+        Parent parent1 = new Parent();
+        System.out.println("parent1 호출");
+        call(parent1);
+        Parent parent2 = new Child();
+        System.out.println("parent2 호출");
+        call(parent2);
+    }
+
+
+    //자바 16부터는 instanceof 를 사용하면서 동시에 변수를 선언할 수 있다.
+    private static void call(Parent parent) {
+        parent.parentMethod();
+        //Child 인스턴스인 경우 childMethod() 실행
+        if (parent instanceof Child child) {
+            System.out.println("Child 인스턴스 맞음");
+            child.childMethod();
+        }
+    }
+
+}
